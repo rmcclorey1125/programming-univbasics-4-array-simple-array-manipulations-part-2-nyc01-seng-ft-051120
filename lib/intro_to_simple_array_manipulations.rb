e2ee7226fa0e3c07be11e44dd9f494fb72a28170
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-04-24 23:14:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,1 +1,17 @@
 def using_concat (array, new_array)
+  array.concat new_array
+end
+
+def using_insert (array, element)
+  array.insert element[4]
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.using_flatten
+end
+
+def using_delete (array, string)
